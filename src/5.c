71780721276ab01a4dd922e260d6a8e5c853410b
cosavalsamma <cosavalsamma@gmail.com>
2025-03-10 11:53:03 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int age = 30;
+    if (age >= 18) {
+        printf("You are eligible to vote!\n");
+    } else {
+        printf("Sorry, you are not eligible to vote yet.\n");
+    }
+    return 0;
+}
