c847b13d55cfc791d191151f111ef94c905aaf08
cosavalsamma <cosavalsamma@gmail.com>
2025-03-19 22:14:53 +0000

    Add 14.c

---
+++ src/14.c
@@ -0,0 +1,11 @@
+int main() {
+    int num1 = 5;
+    int num2 = 7;
+    int result = add(num1, num2);
+    printf("The sum is %d", result);
+    return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
